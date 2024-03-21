993c28296858ff14c4dded1c48135945b95d4880
Marcela Jimenez <150343795+MarcelaJI@users.noreply.github.com>
2024-03-21 10:34:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let btnMenu = document.getElementById('menu_mobile');
+let mainNav = document.getElementById('navigation');
+btnMenu.addEventListener('click', function(){
+ mainNav.classList.toggle('mostrar');
+});
+
